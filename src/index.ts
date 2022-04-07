cb5d8aae5dd1bdff68b1b22710c9429fa5f00774
ThomasM-E2X <thomas.martin@e2x.com>
2022-04-07 14:14:07 +0100

    push-workflow update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import ContentfulClient from "./contentfulClient";
 import { models } from "./models";
 
 const client = new ContentfulClient();
-// client.pushChanges(models);
+client.pushChanges(models);
 // client.removeEnv("GH-refactorContentfulClient");
-client.createEnv();
+// client.createEnv();
