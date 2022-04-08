25ac52a6aef8187fcdd28d9bcb9707bc954a9d5f
ThomasM-E2X <thomas.martin@e2x.com>
2022-04-08 12:40:30 +0100

    push changes

--- src/models/page.ts
+++ src/models/page.ts
@@ -2,6 +2,13 @@ export const Page = {
   name: "page",
   fields: [
     {
+      id: "pageUrl",
+      name: "Page Url",
+      required: true,
+      localized: true,
+      type: "Text",
+    },
+    {
       id: "url",
       name: "url",
       required: true,
